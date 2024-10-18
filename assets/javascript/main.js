a4c68d05f698ff0ff54c8f3688c54e9ab3ba4a4a
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-18 12:21:40 +0200

    refactory with function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,35 +8,32 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
     
     let photoCols = ''
     photos.forEach( photo => {
-        const {album, id, title, url} = photo
-        console.log(id, url);
-        const markup = `
-                        <div class="col">
-                    <div class="card">
-                        <img class="pin" src="./assets/img/pin.svg" alt="">
-                        <div class="imag_card">
-                            <img src="${url}" alt="">
-                        </div>
-                        <!-- /.imag_card -->
-                         <div class="card_body">
-                            ${title}
-                         </div>
-                         <!-- /.card_body -->
-                    </div>
-                </div>
-                <!-- /.col -->
-        `
-        console.log(markup);
-        console.log(rowEl);
+        const {album, id, title, url} = photo        
         
-        
-        photoCols += markup
+        photoCols += markUp(url, id)
     });
     rowEl.innerHTML = photoCols
-    
-
-
-
-
-    
+      
 })
+
+
+function markUp(url, title) {
+
+    const markup = `
+    <div class="col">
+<div class="card">
+    <img class="pin" src="./assets/img/pin.svg" alt="">
+    <div class="imag_card">
+        <img src="${url}" alt="">
+    </div>
+    <!-- /.imag_card -->
+     <div class="card_body">
+        ${title}
+     </div>
+     <!-- /.card_body -->
+</div>
+</div>
+<!-- /.col -->
+`
+    return markup
+}
