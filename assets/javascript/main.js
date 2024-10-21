e245c1d216af70db697dc3cc7a3c24f5d9ba0bab
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-21 13:36:30 +0200

    fix focus text

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -19,71 +19,75 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
         let cardEl
         let imgEl
         let titleEl
-
+        let initial 
         photos.forEach(photo => {
             const { album, id, title, url } = photo
-            count++        
-            photoCols += markUp(url, title, id)
+            count++
+            console.log(title);
+            initial = title.substring(0, 1).toUpperCase()+title.substring(1).toLowerCase()
+            console.log(initial);
             
-        console.log(count);
-        
-            
-            
+            photoCols += markUp(url, initial, id)
+
+            console.log(count);
+
+
+
         });
         insertMarkup(rowEl, photoCols)
         let cardElArr = []
         let imgElArr = []
-        let titleElArr =[]
+        let titleElArr = []
         for (let i = 1; i <= count; i++) {
             cardEl = rowEl.querySelector(`#is${i}`)
             imgEl = document.getElementById(`img${i}`)
-            titleEl = document.getElementById(`title${i}`).innerText.substring(0).toUpperCase()+document.getElementById(`title${i}`).innerText.substring(1).toUpperCase()+
-            
-            console.log(cardEl, imgEl, titleEl);
+            titleEl = document.getElementById(`title${i}`).innerText
+
+                console.log(cardEl, imgEl, titleEl);
             cardElArr.push(cardEl)
             imgElArr.push(imgEl)
             titleElArr.push(titleEl)
-            
+
         }
         console.log(cardElArr);
-        
+
         cardElArr[0].addEventListener('click', function () {
             overlayEl.classList.remove('d-none')
             bodyEl.classList.add('overflow')
             focusImgEl.src = imgElArr[0].src
-        focusCardEl.innerText =titleElArr[0].innerText
+            focusCardEl.innerText = titleElArr[0]
         })
         cardElArr[1].addEventListener('click', function () {
             overlayEl.classList.remove('d-none')
             bodyEl.classList.add('overflow')
             focusImgEl.src = imgElArr[1].src
-        focusCardEl.innerText =titleElArr[1].innerText
+            focusCardEl.innerText = titleElArr[1]
         })
         cardElArr[2].addEventListener('click', function () {
             overlayEl.classList.remove('d-none')
             bodyEl.classList.add('overflow')
             focusImgEl.src = imgElArr[2].src
-        focusCardEl.innerText =titleElArr[2].innerText
+            focusCardEl.innerText = titleElArr[2]
         })
         cardElArr[3].addEventListener('click', function () {
             overlayEl.classList.remove('d-none')
             bodyEl.classList.add('overflow')
             focusImgEl.src = imgElArr[3].src
-        focusCardEl.innerText =titleElArr[3].innerText
+            focusCardEl.innerText = titleElArr[3]
         })
         cardElArr[4].addEventListener('click', function () {
             overlayEl.classList.remove('d-none')
             bodyEl.classList.add('overflow')
             focusImgEl.src = imgElArr[4].src
-        focusCardEl.innerText =titleElArr[4].innerText
+            focusCardEl.innerText = titleElArr[4]
         })
         cardElArr[5].addEventListener('click', function () {
             overlayEl.classList.remove('d-none')
             bodyEl.classList.add('overflow')
             focusImgEl.src = imgElArr[5].src
-        focusCardEl.innerText =titleElArr[5].innerText
+            focusCardEl.innerText = titleElArr[5]
         })
-        
+
         closeEl.addEventListener('click', function () {
             overlayEl.classList.add('d-none')
             bodyEl.classList.remove('overflow')
