81ecf899bbd18d1c69cf4d7487a28904a201286f
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-21 13:31:57 +0200

    add initial to upper case

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -19,9 +19,10 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
         let cardEl
         let imgEl
         let titleEl
+
         photos.forEach(photo => {
             const { album, id, title, url } = photo
-            count++
+            count++        
             photoCols += markUp(url, title, id)
             
         console.log(count);
@@ -36,7 +37,8 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
         for (let i = 1; i <= count; i++) {
             cardEl = rowEl.querySelector(`#is${i}`)
             imgEl = document.getElementById(`img${i}`)
-            titleEl = document.getElementById(`title${i}`)
+            titleEl = document.getElementById(`title${i}`).innerText.substring(0).toUpperCase()+document.getElementById(`title${i}`).innerText.substring(1).toUpperCase()+
+            
             console.log(cardEl, imgEl, titleEl);
             cardElArr.push(cardEl)
             imgElArr.push(imgEl)
@@ -51,18 +53,37 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
             focusImgEl.src = imgElArr[0].src
         focusCardEl.innerText =titleElArr[0].innerText
         })
+        cardElArr[1].addEventListener('click', function () {
+            overlayEl.classList.remove('d-none')
+            bodyEl.classList.add('overflow')
+            focusImgEl.src = imgElArr[1].src
+        focusCardEl.innerText =titleElArr[1].innerText
+        })
+        cardElArr[2].addEventListener('click', function () {
+            overlayEl.classList.remove('d-none')
+            bodyEl.classList.add('overflow')
+            focusImgEl.src = imgElArr[2].src
+        focusCardEl.innerText =titleElArr[2].innerText
+        })
+        cardElArr[3].addEventListener('click', function () {
+            overlayEl.classList.remove('d-none')
+            bodyEl.classList.add('overflow')
+            focusImgEl.src = imgElArr[3].src
+        focusCardEl.innerText =titleElArr[3].innerText
+        })
+        cardElArr[4].addEventListener('click', function () {
+            overlayEl.classList.remove('d-none')
+            bodyEl.classList.add('overflow')
+            focusImgEl.src = imgElArr[4].src
+        focusCardEl.innerText =titleElArr[4].innerText
+        })
+        cardElArr[5].addEventListener('click', function () {
+            overlayEl.classList.remove('d-none')
+            bodyEl.classList.add('overflow')
+            focusImgEl.src = imgElArr[5].src
+        focusCardEl.innerText =titleElArr[5].innerText
+        })
         
-
-
-        function selectImgEl(number) {
-            let imgEl = document.getElementById(`img${number}`)
-            return imgEl
-        }
-
-
-
-
-
         closeEl.addEventListener('click', function () {
             overlayEl.classList.add('d-none')
             bodyEl.classList.remove('overflow')
@@ -98,9 +119,4 @@ function markUp(url, title, id) {
 function insertMarkup(where, what) {
     return where.innerHTML = what
 }
-// function singleCard(number) {
-//     let cardEl = rowEl.querySelector(`#is${number}`)
-//     let imgEl= document.getElementById(`img${number}`)
-//     let titleEl = document.getElementById(`title${number}`)
-//     return cardEl, imgEl, titleEl
-// }
+
