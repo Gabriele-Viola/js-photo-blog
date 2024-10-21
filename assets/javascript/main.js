43d983d8d8b6f215d01f9997d7700d48589b13f2
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-21 13:08:03 +0200

    add for loop

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,6 +4,10 @@ const overlayEl = document.querySelector('.overlay')
 const bodyEl = document.querySelector('body')
 console.log(rowEl, overlayEl);
 
+const closeEl = overlayEl.querySelector('button')
+const focusImgEl = overlayEl.querySelector('img')
+const focusCardEl = overlayEl.querySelector('.focus-card-body')
+console.log(focusImgEl);
 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
     .then(respo => {
@@ -11,62 +15,54 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
         console.log(photos);
 
         let photoCols = ''
+        let count = 0
+        let cardEl
+        let imgEl
+        let titleEl
         photos.forEach(photo => {
             const { album, id, title, url } = photo
-
+            count++
             photoCols += markUp(url, title, id)
+            
+        console.log(count);
+        
+            
+            
         });
         insertMarkup(rowEl, photoCols)
-
-        const closeEl = overlayEl.querySelector('button')
-        const focusImgEl = overlayEl.querySelector('img')
-        const focusCardEl = overlayEl.querySelector('.focus-card-body')
-        console.log(focusImgEl);
-
-
-
-
-
-
-        selecteCard(1).addEventListener('click', function () {
+        let cardElArr = []
+        let imgElArr = []
+        let titleElArr =[]
+        for (let i = 1; i <= count; i++) {
+            cardEl = rowEl.querySelector(`#is${i}`)
+            imgEl = document.getElementById(`img${i}`)
+            titleEl = document.getElementById(`title${i}`)
+            console.log(cardEl, imgEl, titleEl);
+            cardElArr.push(cardEl)
+            imgElArr.push(imgEl)
+            titleElArr.push(titleEl)
+            
+        }
+        console.log(cardElArr);
+        
+        cardElArr[0].addEventListener('click', function () {
             overlayEl.classList.remove('d-none')
             bodyEl.classList.add('overflow')
-            focusImgEl.src = selectImgEl(1).src
-            focusCardEl.innerText = selectTitleEl(1)
+            focusImgEl.src = imgElArr[0].src
+        focusCardEl.innerText =titleElArr[0].innerText
         })
-        selecteCard(2).addEventListener('click', function () {
-            overlayEl.classList.remove('d-none')
-            bodyEl.classList.add('overflow')
+        
 
-            focusImgEl.src = selectImgEl(2).src
-            focusCardEl.innerText = selectTitleEl(2)
 
-        })
-        selecteCard(3).addEventListener('click', function () {
-            overlayEl.classList.remove('d-none')
-            bodyEl.classList.add('overflow')
+        function selectImgEl(number) {
+            let imgEl = document.getElementById(`img${number}`)
+            return imgEl
+        }
 
-            focusImgEl.src = selectImgEl(3).src
-            focusCardEl.innerText = selectTitleEl(3)
 
-        })
-        selecteCard(4).addEventListener('click', function () {
-            overlayEl.classList.remove('d-none')
-            focusImgEl.src = selectImgEl(4).src
-            focusCardEl.innerText = selectTitleEl(4)
-        })
-        selecteCard(5).addEventListener('click', function () {
-            overlayEl.classList.remove('d-none')
-            focusImgEl.src = selectImgEl(5).src
-            focusCardEl.innerText = selectTitleEl(5)
 
-        })
-        selecteCard(6).addEventListener('click', function () {
-            overlayEl.classList.remove('d-none')
-            focusImgEl.src = selectImgEl(6).src
-            focusCardEl.innerText = selectTitleEl(6)
 
-        })
+
         closeEl.addEventListener('click', function () {
             overlayEl.classList.add('d-none')
             bodyEl.classList.remove('overflow')
@@ -108,16 +104,3 @@ function insertMarkup(where, what) {
 //     let titleEl = document.getElementById(`title${number}`)
 //     return cardEl, imgEl, titleEl
 // }
-function selecteCard(number) {
-    let cardEl = rowEl.querySelector(`#is${number}`)
-    return cardEl
-}
-
-function selectImgEl(number) {
-    let imgEl = document.getElementById(`img${number}`)
-    return imgEl
-}
-function selectTitleEl(number) {
-    let titleEl = document.getElementById(`title${number}`)
-    return titleEl.innerText
-}
