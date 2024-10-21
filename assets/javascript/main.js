69d4768fe62266c24ba4560f8a90559df869646e
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-21 10:44:47 +0200

    add remove n add class

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,8 @@
 
 let rowEl = document.querySelector('.row')
+let overlayEl = document.querySelector('.overlay')
+console.log(rowEl, overlayEl);
+
 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
 .then(respo => {
@@ -10,17 +13,34 @@ axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
     photos.forEach( photo => {
         const {album, id, title, url} = photo        
         
-        photoCols += markUp(url, title)
+        photoCols += markUp(url, title, id)
     });
-      insertMarkup(rowEl, photoCols)
+    insertMarkup(rowEl, photoCols)
+    
+    let card1El = rowEl.querySelector('#is1')
+    let card2El = rowEl.querySelector('#is2')
+    let card3El = rowEl.querySelector('#is3')
+    let card4El = rowEl.querySelector('#is4')
+    let card5El = rowEl.querySelector('#is5')
+    let card6El = rowEl.querySelector('#is6')
+    console.log(card1El, card2El,card3El,card4El,card5El,card6El);
+
+    card1El.addEventListener('click', function () {
+     overlayEl.classList.remove('d-none')   
+    })
+    overlayEl.addEventListener('click', function () {
+        overlayEl.classList.add('d-block')   
+       })
+
 })
+    
+    
 
-
-function markUp(url, title) {
+function markUp(url, title, id) {
 
     const markup = `
     <div class="col">
-<div class="card">
+<div id="is${id}" class="card photo${id}">
     <img class="pin" src="./assets/img/pin.svg" alt="">
     <div class="imag_card">
         <img src="${url}" alt="">
@@ -38,5 +58,4 @@ function markUp(url, title) {
 }
 function insertMarkup(where, what) {
    return where.innerHTML = what
-    
 }
